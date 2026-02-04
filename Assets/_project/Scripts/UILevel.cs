1c68d99fef4af3f2586376bd5ca3c738780e828a
Milan Dupuis <milan.dupuis@gmail.com>
2026-02-04 11:32:47 +0100

    [ADD] DicoDino

--- Assets/_project/Scripts/UILevel.cs
+++ Assets/_project/Scripts/UILevel.cs
@@ -1,11 +1,16 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization.Settings;
 using UnityEngine.UI;
 
 public class UILevel : MonoBehaviour
 {
     [Header("Dictionnary")]
     [SerializeField] private GameObject _dictionnary;
+    [SerializeField] private DicoDinoDatabase _databaseDico;
+    [SerializeField] private TextMeshProUGUI _textDico;
     [SerializeField] private Button _buttonDictionnary;
     [SerializeField] private Button _buttonDicoExit;
     [SerializeField] private Button _buttonMainMenu;
@@ -19,10 +24,21 @@ public class UILevel : MonoBehaviour
     [SerializeField] private Button _buttonFrench;
     [SerializeField] private Button _buttonEnglish;
 
+    private void OnEnable()
+    {
+        LocalizationSettings.SelectedLocaleChanged += OnLocaleChanged;
+    }
+
+    private void OnDisable()
+    {
+        LocalizationSettings.SelectedLocaleChanged -= OnLocaleChanged;
+    }
+
     public void Initialize()
     {
         UIManager.Instance.SetActiveMenu((_dictionnary, true), (_parameters, true));
 
+        _textDico.GetComponent<LocalizeStringEvent>().OnUpdateString.RemoveAllListeners();
         _buttonDictionnary.onClick.RemoveAllListeners();
         _buttonDicoExit.onClick.RemoveAllListeners();
         _buttonMainMenu.onClick.RemoveAllListeners();
@@ -32,6 +48,7 @@ public class UILevel : MonoBehaviour
         _buttonFrench.onClick.RemoveAllListeners();
         _buttonEnglish.onClick.RemoveAllListeners();
 
+        _textDico.GetComponent<LocalizeStringEvent>().OnUpdateString.AddListener(_ => UpdateDicoDino());
         _buttonDictionnary.onClick.AddListener(() => UIManager.Instance.SetActiveMenu((_dictionnary, true)));
         _buttonMainMenu.onClick.AddListener(() => ScenesManager.Instance.LoadSceneMainMenu());
         _buttonLevelSelect.onClick.AddListener(() => ScenesManager.Instance.LoadSceneMainMenu(true));
@@ -43,8 +60,30 @@ public class UILevel : MonoBehaviour
         _buttonParamExit.onClick.AddListener(() => UIManager.Instance.SetActiveMenu((_parameters, false)));
         _buttonDicoExit.onClick.AddListener(() => UIManager.Instance.SetActiveMenu((_dictionnary, false)));
 
+        UpdateDicoDino();
+
         UIManager.Instance.InitializeTextTranslate();
 
         UIManager.Instance.SetActiveMenu((_dictionnary, false), (_parameters, false));
+    }
+
+    private void UpdateDicoDino()
+    {
+        var table = LocalizationSettings.StringDatabase.GetTable("DicoDino");
+        _textDico.text = string.Empty;
+
+        foreach (var word in _databaseDico.Dictionnary)
+        {
+            var entry = table.GetEntry(word.KeyUniversalTraduction);
+            if (entry == null)
+                continue;
+
+            _textDico.text += word.DinoWord + " : " + entry.GetLocalizedString() + "\n";
+        }
+    }
+
+    private void OnLocaleChanged(Locale locale)
+    {
+        UpdateDicoDino();
     }
 }

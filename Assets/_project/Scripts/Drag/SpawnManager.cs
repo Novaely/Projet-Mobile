6423a69940bd794d9fd0b35b14151b4c11ad24e5
Ely <adam.guerfi06@gmail.com>
2026-02-11 19:59:26 +0100

    [Add] new system dino spawn

--- Assets/_project/Scripts/Drag/SpawnManager.cs
+++ Assets/_project/Scripts/Drag/SpawnManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,11 +7,24 @@ public class SpawnManager : MonoBehaviour
 {
     public static SpawnManager Instance;
 
-    [SerializeField] Seat _spawnerSeat; 
-    [SerializeField] List<GameObject> _dinos;
+    [SerializeField] Seat _spawnerSeat;
+    [SerializeField] List<GameObject> _dinosPrefab;
+    List<GameObject> _dinos = new();
     [SerializeField] Transform _dinosContainer;
 
     public event Action <Dino, Transform> OnSpawn;
+
+    int _currentIdDino = -1;
+
+    public class InfoDino
+    {
+        public string label;
+        public string contraintePositive;
+        public string contrainteNegative;
+        public Sprite sprite;
+    }
+
+    UIInfoDinoLevel _UIInfoDino;
 
     void Awake()
     {
@@ -18,15 +32,104 @@ public class SpawnManager : MonoBehaviour
         Instance = this;
     }
 
+    private void Start()
+    {
+        StartCoroutine(WaitStart());
+
+        IEnumerator WaitStart()
+        {
+            yield return new WaitForEndOfFrame();
+            _UIInfoDino = FindFirstObjectByType<UIInfoDinoLevel>();
+
+            _UIInfoDino.OnNextDino += NextDino;
+            _UIInfoDino.OnPreviousDino += PrevDino;
+
+            for (int i = 0; i < _dinosPrefab.Count; i++)
+            {
+                GameObject dino = Instantiate(_dinosPrefab[i], _spawnerSeat.transform.position, Quaternion.identity, _dinosContainer);
+
+                if (i == 0)
+                {
+                    dino.SetActive(false);
+                    Dino dinoDino = dino.GetComponent<Dino>();
+                    OnSpawn?.Invoke(dinoDino, _spawnerSeat.transform);
+                }
+
+                _dinos.Add(dino);
+            }
+
+            _UIInfoDino.NextDino();
+        }
+    }
+
     private void Update()
     {
-        if (_dinos.Count == 0) return;
+        if (_currentIdDino == -1) { return; }
+
+        if (_dinos.Count == 1)
+        {
+            return;
+        }
+
         if (_spawnerSeat != null && _spawnerSeat.occupant == null)
         {
-            GameObject item = Instantiate(_dinos[0], _spawnerSeat.transform.position, Quaternion.identity, _dinosContainer);
-            Dino dino = item.GetComponent<Dino>();
-            OnSpawn?.Invoke(dino, _spawnerSeat.transform);
-            _dinos.RemoveAt(0);
+            _dinos.RemoveAt(_currentIdDino);
+            _UIInfoDino.NextDino();
         }
+    }
+
+    InfoDino NextDino()
+    {
+        if (_currentIdDino == -1)
+        {
+            for (int i = 1; i < _dinos.Count; i++)
+            {
+                _dinos[i].SetActive(false);
+            }
+        }
+        if (_dinos.Count >= 1 && _currentIdDino != -1)
+        {
+            _dinos[_currentIdDino].SetActive(false);
+        }
+
+        _currentIdDino = (_currentIdDino + 1) % _dinos.Count;
+
+        GameObject dino = _dinos[_currentIdDino];
+
+        InfoDino infoDino = new InfoDino
+        {
+            label = dino.name,
+            contraintePositive = "",
+            contrainteNegative = "",
+            sprite = dino.GetComponent<SpriteRenderer>().sprite,
+        };
+
+        _dinos[_currentIdDino].SetActive(true);
+
+        Dino dinoDino = dino.GetComponent<Dino>();
+        OnSpawn?.Invoke(dinoDino, _spawnerSeat.transform);
+
+        return infoDino;
+    }
+
+    InfoDino PrevDino()
+    {
+        _dinos[_currentIdDino].SetActive(false);
+
+        _currentIdDino = (_currentIdDino - 1) % _dinos.Count;
+
+        GameObject dino = _dinos[_currentIdDino];
+
+        InfoDino infoDino = new InfoDino
+        {
+            label = dino.name,
+            contraintePositive = "",
+            contrainteNegative = "",
+            sprite = dino.GetComponent<SpriteRenderer>().sprite,
+        };
+
+        _dinos[_currentIdDino].SetActive(true);
+
+        return infoDino;
     }
 }

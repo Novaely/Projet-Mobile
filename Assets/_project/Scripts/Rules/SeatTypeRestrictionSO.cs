e9a9a7a0af44fb7b1840ad1c94152fbc4f219424
varlet-enzo <enzo.varlet.pro@gmail.com>
2026-02-11 10:17:08 +0100

    [FIX] tools & rules

--- Assets/_project/Scripts/Rules/SeatTypeRestrictionSO.cs
+++ Assets/_project/Scripts/Rules/SeatTypeRestrictionSO.cs
@@ -1,27 +1,12 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "SeatRules/Seat Type Restriction")]
+[CreateAssetMenu(menuName = "Rules/Seat Type Restriction")]
 public class SeatTypeRestrictionSO : SeatRuleSO
 {
-    [Header("Quel type de siège ?")]
-    public SeatType targetSeatType;
-
-    [Header("Conditions requises")]
-    public bool mustBeHerbivore;
-    public bool mustBeCarnivore;
-    public bool mustBeColor;
-    public DinoColor requiredColor;
+    public DietType allowedDiet;
 
     public override bool IsSatisfied(Dino dino, Seat seat)
     {
-        if (seat.seatType != targetSeatType) return true;
-
-        if (dino == null) return false;
-
-        if (mustBeHerbivore && dino.diet != DietType.Herbivore) return false;
-        if (mustBeCarnivore && dino.diet != DietType.Carnivore) return false;
-        if (mustBeColor && dino.color != requiredColor) return false;
-
-        return true;
+        return dino.diet == allowedDiet;
     }
 }
